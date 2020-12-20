8d1a847f23352d663c11c930d8e0381000692efb
thewh1teagle <moshvilla@yandex.com>
2020-12-20 19:37:31 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="nwpy",  # How you named your package folder
-    packages=["src"],  # Chose the same as "name"
+    packages=["nwpy"],  # Chose the same as "name"
     include_package_data=True,
     version="v0.2",  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
